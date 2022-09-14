8fe904ec54c09c4da077673cef359ef1250cc60a
aldifzi <faaldy16@gmail.com>
2022-09-14 19:39:48 +0700

    apdet

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ words.forEach(word => {
   tl.to('.text', {duration: 1, text: word})
   masterTl.add(tl)
 })
+
+
+
+window.onload = function() { jam(); }
+       
+        function jam() {
+            var e = document.getElementById('jam'),
+            d = new Date(), h, m, s;
+            h = d.getHours();
+            m = set(d.getMinutes());
+            s = set(d.getSeconds());
+       
+            e.innerHTML = h +':'+ m +':'+ s;
+       
+            setTimeout('jam()', 1000);
+        }
+       
+        function set(e) {
+            e = e < 10 ? '0'+ e : e;
+            return e;
+        }
