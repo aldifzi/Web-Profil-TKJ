9a9be6a3acb21fec01f8a1ca12a98b7077ef6128
aldifzi <faaldy16@gmail.com>
2022-10-24 15:02:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -297,9 +297,9 @@ $page = 'home';
           </div>
 
           <div class="progress">
-            <span class="skill">Microtik<i class="val">85%</i></span>
+            <span class="skill">Microtik<i class="val">5%</i></span>
             <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100">
               </div>
             </div>
           </div>
