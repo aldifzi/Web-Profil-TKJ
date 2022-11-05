f007cfb8a7b1e393dc00f7141714ba6f5760ff61
aldifzi <faaldy16@gmail.com>
2022-11-06 00:29:25 +0700

    Update proses_tambah-guru.php

--- assets/admin/proses_tambah-guru.php
+++ assets/admin/proses_tambah-guru.php
@@ -6,12 +6,11 @@ require_once '../../koneksi.php';
 $nama = mysqli_escape_string($connection, $_POST['nama']);
 $nip = mysqli_escape_string($connection, $_POST['nip']);
 $jenis_kelamin = mysqli_escape_string($connection, $_POST['jenis_kelamin']);
-$no_hp = mysqli_escape_string($connection, $_POST['no_hp']);
+$hp = mysqli_escape_string($connection, $_POST['hp']);
 $tempat_lahir = mysqli_escape_string($connection, $_POST['tempat_lahir']);
-$tanggal_lahir = mysqli_escape_string($connection, $_POST['tanggal_lahir']);
+$tanggal_lahir = mysqli_escape_string($connection, $_POST['tangal_lahir']);
 $mata_pelajaran = mysqli_escape_string($connection, $_POST['mata_pelajaran']);
 $alamat = mysqli_escape_string($connection, $_POST['alamat']);
-$foto = mysqli_escape_string($connection, $_POST['foto']);
 
 // persiapan upload foto
 $ekstensi = $_FILES['foto']['name'];
@@ -21,13 +20,13 @@ $nama_foto = strtolower($nama);
 $nama_foto = str_replace(' ', '-', $nama_foto) . '.' . $ekstensi;
 
 $asal = $_FILES['foto']['tmp_name'];
-$tujuan = '../../images/guru/';
+$tujuan = './../../';
 
 if($_FILES['foto']['error'] == 0){
 	if($_FILES['foto']['size'] < 1000000){
 		if (file_exists($tujuan . $nama_foto)) unlink($tujuan . $nama_foto);
 
-		$query = mysqli_query($connection, "INSERT INTO tbl_guru (nama, nip, jenis_kelamin, no_hp, tempat_lahir, tanggal_lahir, mata_pelajaran, alamat, foto) VALUES('$nama', $nip, '$jenis_kelamin', '$no_hp', '$tempat_lahir', '$tanggal_lahir', '$mata_pelajaran', '$alamat', '$nama_foto')") or die(mysqli_error($connection));
+		$query = mysqli_query($connection, "INSERT INTO tguru (nama, nip, jenis_kelamin, hp, tempat_lahir, tangal_lahir, mata_pelajaran, alamat, foto) VALUES('$nama', $nip, '$jenis_kelamin', '$hp', '$tempat_lahir', '$tanggal_lahir', '$mata_pelajaran', '$alamat', '$nama_foto')") or die(mysqli_error($connection));
 		move_uploaded_file($asal, $tujuan . $nama_foto) or die('gagal upload foto');
 		if($query){
 			$_SESSION['sukses'] = 'Data Berhasil Ditambahkan!';
